cd79c7d447ab56d0eeb538deac6c63f9df1a5f5d
Data | Computer Scientist <60397852+Cython-dot@users.noreply.github.com>
2020-04-05 10:36:01 +0200

    Hello World
    // Simple program showing on console -> Hello World, my name is Dorian.

---
+++ Hello World.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Simple program showing on console -> Hello World, my name is Dorian.
+int main(void)
+{
+    printf("Hello World, my name is Dorian.");
+    return 0;
+}
+
