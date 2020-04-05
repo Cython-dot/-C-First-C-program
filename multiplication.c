0caad03faa661725fe854efeadd6e2d8cb1de54c
Data | Computer Scientist <60397852+Cython-dot@users.noreply.github.com>
2020-04-05 10:44:57 +0200

    int multiplication()
    // The fonction multiplication() enables the user to perform a multiplication.

---
+++ multiplication.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// The fonction multiplication() enables the user to perform a multiplication. 
+
+int multiplication(int x, int y)
+{
+    return x * y;
+}
