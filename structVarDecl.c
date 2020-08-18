3c9daf38e8a3bb7768d787f76041536c7104a744
Data | Computer Scientist <60397852+Cython-dot@users.noreply.github.com>
2020-08-18 23:14:34 +0200

    Defining the struct + a variable in one shot
    Valid to declare a variable to be of a particular structure type at the same time that the structure is defined. 
    Incl. the variable name before the terminating ';' of the structure definition.

---
+++ structVarDecl.c
@@ -0,0 +1,14 @@
+struct date 
+{
+  
+  int day;
+  int month;
+  int year;
+ 
+} tomorrow;
+
+                             /*
+      In the above, an instance of the structure, called tomorrow, 
+      is declared simultaneously with the structure. 
+      Tomorrow is a variable of type date.
+						                */
