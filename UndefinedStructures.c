d5219a45ba9cbcd06aa21a02e274ad2e36630e04
Data | Computer Scientist <60397852+Cython-dot@users.noreply.github.com>
2020-08-19 08:52:58 +0200

    Undefined structures
    It's not necessary to provide a tag name to your struct. You could declare it undefined. A disadvantage : you can no longer define further instances of the structure

---
+++ UndefinedStructures.c
@@ -0,0 +1,15 @@
+struct
+{              // struct declaration 
+
+  int day;
+  int month;
+  int year;
+  
+} tomorrow;    // struct variable declaration combined 
+
+                              /*
+                              
+  All the variables of this structure type you want in your program 
+  must be defined in the one statement> 
+  
+                              */ 
