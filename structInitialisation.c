57abf1cc768ea7ef7c32cce1cb35b4d66ce6a73b
Data | Computer Scientist <60397852+Cython-dot@users.noreply.github.com>
2020-08-19 08:28:45 +0200

    Initialising structures
    Initialising structures is like initialising arrays. 
    Elements are listed inside curly braces, and each one is separated by a comma. 
    The initial values listed inside the curly braces must be constant expressions.

---
+++ structInitialisation.c
@@ -0,0 +1,11 @@
+// Full initialisation
+
+struct date dateOne = {20, 8, 2020}; 
+
+// Like an array initialisation, fewer values might be contained in the structure
+
+struct date dateOne = {20, 8};
+
+// sets dateOne.day to 20 and dateOne.month to 8 but gives no initial value to date.year
+                        
+                              
